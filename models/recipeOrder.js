abecd833288a8b81e9839dbcd4b6e6d1954aaf43
Kohchangjue <kohchangjue@gmail.com>
2021-01-16 20:25:31 +0800

    delete unaccessary required

--- models/recipeOrder.js
+++ models/recipeOrder.js
@@ -9,19 +9,15 @@ const recepiOrderSchema = new mongoose.Schema(
         },
         title: {
             type: String,
-            required:true
         },
         recipe:{
             type: String,
-            required:true
         },
         price:{
             type: String,
-            required:true
         },
         status: {
             type:String,
-            required: true,
             enum: ['1','2','3','4','5','6','7']
         },
         message:{
